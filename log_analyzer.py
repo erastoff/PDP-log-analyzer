4074f61881b455a3a8b098b7510785657d978418
erastoff <yuriy.erastov@yandex.ru>
2023-12-03 16:08:04 +0300

    Master - Fixed functions and added report_dir creation

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
 import json
+import os
 import re
-from json import loads, dumps, dump
 
 import pandas as pd
-from jinja2 import FileSystemLoader, Environment
+from jinja2 import Environment, FileSystemLoader
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -16,7 +17,6 @@ config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 def reader(config: dict):
-    # with open("./log/nginx-access-ui.log-20170630", 'r') as f:
     with open(config.get("LOG_DIR") + "/nginx-access-ui.log-20170630", "r") as f:
         data = f.readlines()
     return data
@@ -33,13 +33,7 @@ def parse_log_line(line: str):
         return None
 
 
-def truncate_string(text, max_length=30):
-    return text[:max_length] + ("..." if len(text) > max_length else "")
-
-
-def main():
-    d = reader(config)
-
+def create_dataframe(strings: list) -> dict:
     columns = [
         "$remote_addr",
         "$remote_user",
@@ -58,7 +52,7 @@ def main():
         "$request_time",
     ]
 
-    data = [parse_log_line(line) for line in d[:] if line]
+    data = [parse_log_line(line) for line in strings[:300000] if line]
 
     df = pd.DataFrame(data, columns=columns)
 
@@ -77,7 +71,7 @@ def main():
     time_max = df.groupby(by=["$request_path"])["$request_time"].max()
     time_med = df.groupby(by=["$request_path"])["$request_time"].median()
 
-    frame = {
+    return {
         "count": count,
         "count_perc": count_perc,
         "time_sum": time_sum,
@@ -87,23 +81,37 @@ def main():
         "time_med": time_med,
     }
 
+
+def truncate_string(text, max_length=30):
+    return text[:max_length] + ("..." if len(text) > max_length else "")
+
+
+def main():
+    d = reader(config)
+
+    frame = create_dataframe(d)
+
     df1 = pd.DataFrame(frame).sort_values(by="time_sum", ascending=False).head(1000)
 
-    unparsed_json = (
+    # json is written to html-template report-template.html
+    unparsed_json = json.loads(
         df1.reset_index()
         .rename(columns={"$request_path": "url"})
         .to_json(orient="records")
     )
-    json_data_raw = loads(unparsed_json)
-    json_data = json.dumps(json_data_raw)
-
+    json_data = json.dumps(unparsed_json)
     env = Environment(loader=FileSystemLoader("."))
-    template = env.get_template("/report-template.html")
-
+    template = env.get_template("/static/html/report-template.html")
     rendered_html = template.render(json_data=json_data)
 
-    with open("report.html", "w") as output_file:
-        output_file.write(rendered_html)
+    # html-report creation
+    try:
+        with open(config.get("REPORT_DIR") + "/report.html", "w") as output_file:
+            output_file.write(rendered_html)
+    except FileNotFoundError:
+        os.mkdir(config.get("REPORT_DIR"))
+        with open(config.get("REPORT_DIR") + "/report.html", "w") as output_file:
+            output_file.write(rendered_html)
 
 
 if __name__ == "__main__":
