6e6cb8f1b8d3d230c2ed67a7a41920ea540e2134
erastoff <yuriy.erastov@yandex.ru>
2023-12-04 23:01:24 +0300

    Master - reader func updated (gzip, report naming, and log's file reading)

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import argparse
+import datetime as DT
+import gzip
 import json
 import os
+
 import re
 
 import pandas as pd
@@ -17,9 +21,54 @@ config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 def reader(config: dict):
-    with open(config.get("LOG_DIR") + "/nginx-access-ui.log-20170630", "r") as f:
-        data = f.readlines()
-    return data
+    files = []
+    log_prefix = "nginx-access-ui.log"
+    date_pattern = re.compile(r"\d{8}")
+    # get file names in LOG_DIR
+    for file in os.listdir(config.get("LOG_DIR")):
+        if log_prefix in file:
+            files.append(file)
+    if not bool(files):
+        print("The are no files to parse!")
+        exit(0)
+
+    # file selection
+    last_file = files[0]  # initial file name
+    last_date = DT.datetime.min  # initial file date
+    for name in files:
+        match = date_pattern.search(name)
+        if match:
+            date_str = match.group()
+            dt = DT.datetime.strptime(date_str, "%Y%m%d")
+            if dt > last_date:
+                last_file = name
+                last_date = dt
+        else:
+            print(f"Incorrect data suffix in log name: '{name}'")
+            continue
+    if last_date == DT.datetime.min:
+        print("The are no files to parse!")
+        exit(0)
+    # report file name generation and check its existence
+    report_name = "report-" + last_date.strftime("%Y.%m.%d") + ".html"
+    if report_name in os.listdir(config.get("REPORT_DIR")):
+        print(f"Log file '{last_file}' has been already parsed!")
+        exit(0)
+
+    # file opening
+    if ".gz" in last_file:
+        with gzip.open(
+            config.get("LOG_DIR") + "/" + last_file, "rt", encoding="utf-8"
+        ) as f:
+            data = f.readlines()
+    else:
+        with open(
+            config.get("LOG_DIR") + "/" + last_file,
+            "rt",
+            encoding="utf-8",
+        ) as f:
+            data = f.readlines()
+    return data, report_name
 
 
 def parse_log_line(line: str):
@@ -52,7 +101,7 @@ def create_dataframe(strings: list) -> dict:
         "$request_time",
     ]
 
-    data = [parse_log_line(line) for line in strings[:] if line]
+    data = [parse_log_line(line) for line in strings[:30000] if line]
 
     df = pd.DataFrame(data, columns=columns)
 
@@ -116,13 +165,14 @@ def main():
     config.update(file_config)
 
     # log-file operations
-    d = reader(config)
-    frame = create_dataframe(d)
+    data, report_name = reader(config)
+    frame = create_dataframe(data)
     df1 = (
         pd.DataFrame(frame)
         .sort_values(by="time_sum", ascending=False)
         .head(config.get("REPORT_SIZE"))
     )
+
     # json is written to html-template report-template.html
     unparsed_json = json.loads(
         df1.reset_index()
@@ -133,13 +183,14 @@ def main():
     env = Environment(loader=FileSystemLoader("."))
     template = env.get_template("/static/html/report-template.html")
     rendered_html = template.render(json_data=json_data)
+
     # html-report creation
     try:
-        with open(config.get("REPORT_DIR") + "/report.html", "w") as output_file:
+        with open(config.get("REPORT_DIR") + "/" + report_name, "w") as output_file:
             output_file.write(rendered_html)
     except FileNotFoundError:
         os.makedirs(config.get("REPORT_DIR"))
-        with open(config.get("REPORT_DIR") + "/report.html", "w") as output_file:
+        with open(config.get("REPORT_DIR") + "/" + report_name, "w") as output_file:
             output_file.write(rendered_html)
 
 
